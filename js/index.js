d45bef3389fb9a93b2eb7ec3ff1dbea4e3357341
raul <rcr_032@hotmail.com>
2022-12-24 12:47:36 +0100

    dias semana

--- js/index.js
+++ js/index.js
@@ -68,24 +68,57 @@ function muestraDatos(){
     document.getElementById('wind').innerHTML= datos.viento + " m/s";
 
     //Mañana
+    let mañana= datos.proximos_dias[0];
+    let fecha= datos.proximos_dias[0]["@attributes"].fecha;
+    let date= new Date(fecha);
+    let dia= date.getDay();   
 
-
-
+    document.getElementById('forecast-day-1-name').innerHTML= diaSemana(dia);
 
     //Pasado
+    let pasado= datos.proximos_dias[1];
+    fecha= datos.proximos_dias[1]["@attributes"].fecha;
+    date= new Date(fecha);
+    dia= date.getDay();   
 
+    document.getElementById('forecast-day-2-name').innerHTML= diaSemana(dia);
 
 
     //Dentro de 3 días
+    let dentro3Dias= datos.proximos_dias[2];
+    fecha= datos.proximos_dias[2]["@attributes"].fecha;
+    date= new Date(fecha);
+    dia= date.getDay();   
 
-
-
+    document.getElementById('forecast-day-3-name').innerHTML= diaSemana(dia);
 
 
     //Dentro de 4 días
+    let dentro4Dias= datos.proximos_dias[3];
+    fecha= datos.proximos_dias[3]["@attributes"].fecha;
+    date= new Date(fecha);
+    dia= date.getDay();   
 
+    document.getElementById('forecast-day-4-name').innerHTML= diaSemana(dia);
 }
 
 
-
+/**
+ * Función que devuelve el día de la semana de una fecha
+ * @param {*} dia 
+ * @returns 
+ */
+function diaSemana(dia){
+    let result="";
+    switch(dia){
+        case 0: result= 'Dom';break;
+        case 1: result= 'Lun';break;
+        case 2: result= 'Mar';break;
+        case 3: result= 'Mie';break;
+        case 4: result= 'Jue';break;
+        case 5: result= 'Vie';break;
+        case 6: result= 'Dom';break;
+    }
+    return result;
+}
 
